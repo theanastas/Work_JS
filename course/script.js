d04dfb2ab4164c7e4e6724aa8a46be25d79990a7
Mansur <svetlanakachura12@gmail.com>
2022-09-11 19:47:57 +0300

    1 commit

---
+++ course/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm("Данные буду потеряны. Вы уверены, что хотите покинуть страницу?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "Да");
+//console.log((answer + 5));
+
+const answers = [];
+
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваше фамилия?', '');
+//answers[2] = prompt('Как ваше отчество?', '');
+
+console.log(typeof(answers));
+
+let a = 10, b = 10;
+a++;
+b--;
+console.log(a);
+console.log(b);
